67dea811c0c2758d0772df69e5b3c77cf5a509bf
matthewtregg <tregearmatthew@gmail.com>
2019-10-04 08:46:59 +0100

    refactor:weatherInfo

--- src/components/WeatherInfo/WeatherInfo.js
+++ src/components/WeatherInfo/WeatherInfo.js
@@ -1,29 +1,17 @@
 import React from "react";
 import "./WeatherInfo.css";
 const WeatherInfo = ({weatherData, metricTemp}) => {
-  let weatherDetails = "";
-  if (weatherData && metricTemp) {
-    weatherDetails = <div className="weatherDetails">
+ 
+  const weatherDetails = weatherData ?
+   <div className="weatherDetails">
       <p>{weatherData.name}</p>
-      <p>{`${weatherData.CelsiusTemp} ${String.fromCharCode(176)} C`}</p>
+      <p>{`${metricTemp? weatherData.CelsiusTemp:weatherData.KelvinTemp} ${String.fromCharCode(176)} C`}</p>
       <p>{`${weatherData.main.pressure} hpa`}</p>
       <p>{`${weatherData.main.humidity} %`}</p>
-    </div>;
-  } else if (weatherData && !metricTemp) {
-    weatherDetails = <div className="weatherDetails">
-      <p>{weatherData.name}</p>
-      <p>{`${weatherData.KelvinTemp} K`}</p>
-      <p>{`${weatherData.main.pressure} hpa`}</p>
-      <p>{`${weatherData.main.humidity} %`}</p>
-    </div>;
-  } else {
-    weatherDetails = null;
-  }
-  
-  
+    </div>: null;
   
   return ( 
-    weatherDetails
+   weatherDetails
   );
 }
 
